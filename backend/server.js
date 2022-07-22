b923ea1e79549024899403694aa1f9c0b0d48670
Jason Thompson <jason@jasont.us>
2022-07-22 14:35:13 -0400

    add cors

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,13 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors())
 
 app.use('/api/auth', require('./routes/authRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
