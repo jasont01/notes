1e85e54f8dfa26afd8e1140a32317d6044039873
Jason Thompson <jason@jasont.us>
2022-07-23 18:18:29 -0400

    fix cookies / cors

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const cookieParser = require('cookie-parser')
 
 const app = express()
 
-app.use(cors())
+app.use(cors({ origin: true, credentials: true }))
 app.use(express.json())
 app.use(cookieParser())
 
