c18d8f851c4beb22b508f56e15cce162b3ad817c
Jason Thompson <jason@jasont.us>
2022-07-24 10:26:25 -0400

    return updated note

--- backend/api/controllers/noteController.js
+++ backend/api/controllers/noteController.js
@@ -50,8 +50,9 @@ const updateNote = async (req, res) => {
   const isValid = await validateRequest(req, res)
 
   if (isValid) {
-    const note = await Note.findByIdAndUpdate(req.params.id, { ...req.body })
-    res.status(200).json(note)
+    await Note.findByIdAndUpdate(req.params.id, { ...req.body })
+    const updatedNote = await Note.findById(req.params.id)
+    res.status(200).json(updatedNote)
   }
 }
 
