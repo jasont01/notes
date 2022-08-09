5fc9fd853e19559c9f7b6fc1548a7b86f2083872
Jason Thompson <jason@jasont.us>
2022-08-09 19:58:40 -0400

    remove base url

--- client/src/api/notesAPI.js
+++ client/src/api/notesAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const notesAPI = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL}/notes`,
+  baseURL: '/api/notes',
 })
 
 const createNote = async (accessToken, data) => {
--- client/src/api/usersAPI.js
+++ client/src/api/usersAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const usersAPI = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL}/users`,
+  baseURL: '/api/users',
 })
 
 const registerUser = async (data) => {
