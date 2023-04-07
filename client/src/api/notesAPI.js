a8209415dbb07ab59567c5e44a7cb5b521d795d4
Jason Thompson <jason@jasont.us>
2023-04-07 08:30:52 -0400

    migrate to vite

--- client/src/api/notesAPI.js
+++ client/src/api/notesAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const notesAPI = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL}/notes`,
+  baseURL: `${import.meta.env.VITE_API_URL}/notes`,
 })
 
 const createNote = async (accessToken, data) => {
--- client/src/api/usersAPI.js
+++ client/src/api/usersAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const usersAPI = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL}/users`,
+  baseURL: `${import.meta.env.VITE_API_URL}/users`,
 })
 
 const registerUser = async (data) => {
